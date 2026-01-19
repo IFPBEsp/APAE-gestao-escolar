bd5d1bba7ed0b8095eebf91cb1871a1682c373ab
emanuelleGued <manugguedes1701@gmail.com>
2026-01-19 15:48:07 -0300

    Revert "Merge pull request #112 from IFPBEsp/111-feature-implementar-a-página-de-alunos-da-barra-lateral-quando-entrar-como-admin"
    This reverts commit 8d82385def8aad1d256fd28ca89c1ced86563c09, reversing
    changes made to baed1a5d65ded42d7ea2e4cda4161dfa7490a2c2.

--- api/src/main/java/com/apae/gestao/repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apae.gestao.repository;
-
-import com.apae.gestao.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
