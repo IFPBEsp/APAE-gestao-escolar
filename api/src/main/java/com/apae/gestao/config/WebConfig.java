b0dfeef78343ed709a568f15a537cc23f3ad240c
emanuelleGued <manugguedes1701@gmail.com>
2026-01-21 22:43:55 -0300

    feat: adicionando permissao das urls no cors config

--- api/src/main/java/com/apae/gestao/config/WebConfig.java
+++ api/src/main/java/com/apae/gestao/config/WebConfig.java
@@ -9,10 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("https://apae-gestao-escolar.vercel.app", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+    registry.addMapping("/**") 
+                .allowedOrigins(
+                    "https://apae-gestao-escolar.vercel.app", 
+                    "https://apae-gestao-escolar.onrender.com", 
+                    "http://localhost:3000"
+                )
+                .allowedMethods("*")
                 .allowedHeaders("*")
+                .exposedHeaders("Authorization") 
                 .allowCredentials(true);
     }
 }
