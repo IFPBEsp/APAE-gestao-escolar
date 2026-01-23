6350d012b832c4e4b310c814f68906b53c3027cc
emanuelleGued <manugguedes1701@gmail.com>
2026-01-22 21:58:39 -0300

    refactor: retirando o header da pagina de login

---
+++ app/src/components/LayoutClientWrapper.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+
+export default function LayoutClientWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === '/';
+
+  return (
+    <main className={`min-h-screen ${!isLoginPage ? 'mt-16 md:mt-20' : ''}`}>
+      {children}
+    </main>
+  );
+}
