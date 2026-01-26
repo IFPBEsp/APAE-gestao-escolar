8f2273c0a969105fa6e8dde5ad8aeb92eb42ca2f
emanuelleGued <manugguedes1701@gmail.com>
2026-01-26 17:04:27 -0300

    Revert "Merge pull request #176 from IFPBEsp/172-feature-implementaçao-do-modulo-de-frequencia---front"
    This reverts commit 26457ec400d8091bebc84bc7a284e7b15a99e492, reversing
    changes made to 49ee4bc4e3f261d3e43b0162834dd07eb20dfb7c.

---
+++ app/src/types/turma.ts
@@ -0,0 +1,19 @@
+import { Professor } from './professor';
+
+export interface TurmaAluno {
+  alunoId: number;
+  nome: string;
+  isAtivo: boolean;
+}
+
+export interface Turma {
+  id: number;
+  nome: string;
+  anoCriacao: number;
+  turno: string;
+  tipo: string;
+  isAtiva: boolean;
+  professor?: Professor;
+  alunos: TurmaAluno[];
+  horario: string;
+}
