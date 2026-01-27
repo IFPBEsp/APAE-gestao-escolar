afb3bfd2e3c1ea8de9969b5b33534df360d0f2e9
João Gabriel Costa <90644849+JoaoGabrielCosta2004@users.noreply.github.com>
2026-01-27 00:40:34 -0300

    chore: removendo import não utilizado

--- api/src/main/java/com/apae/gestao/dto/turma/TurmaResponseDTO.java
+++ api/src/main/java/com/apae/gestao/dto/turma/TurmaResponseDTO.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 import com.apae.gestao.dto.ProfessorResponseDTO;
 import com.apae.gestao.dto.turmaAluno.TurmaAlunoResponseDTO;
 import com.apae.gestao.entity.Turma;
-import com.apae.gestao.entity.TurmaAluno;
-
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
